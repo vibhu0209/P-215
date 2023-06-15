f7c9790a726664a7f9cfa1d98e24cc754f2c0e03
VAIBHAV JAIN <64785307+vibhu0209@users.noreply.github.com>
2023-06-15 15:44:42 +0530

    Add files via upload

---
+++ PRO-C215-Project-Boilerplate-main/server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+server.listen(3030);
