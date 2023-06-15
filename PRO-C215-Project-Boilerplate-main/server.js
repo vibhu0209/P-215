adfe43e2740e755e3b6d6cb6f0dd0faefb4359b0
VAIBHAV JAIN <64785307+vibhu0209@users.noreply.github.com>
2023-06-15 15:45:11 +0530

    Update server.js

--- PRO-C215-Project-Boilerplate-main/server.js
+++ PRO-C215-Project-Boilerplate-main/server.js
@@ -4,4 +4,10 @@ const server = require("http").Server(app);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+
 server.listen(3030);
